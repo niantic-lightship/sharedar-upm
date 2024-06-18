cf4d938b807811be40b791028f670c12e7ff074d
David Lai <davidlai@nianticlabs.com>
2024-06-18 16:47:54 -0700

    Update sharedar-upm for ARDK 3.6

--- Runtime/Metadata.cs
+++ Runtime/Metadata.cs
@@ -4,6 +4,6 @@ namespace Niantic.Lightship.SharedAR.Settings
 {
     public static class Metadata
     {
-        public const string SharedArVersion = "3.5.0-2405101709";
+        public const string SharedArVersion = "3.6.0-2406141834";
     }
 }
