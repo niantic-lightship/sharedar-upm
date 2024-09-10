018647d4e73e9a9c5330d0716a351fe7b7cd4978
David Lai <davidlai@nianticlabs.com>
2024-09-09 17:30:32 -0700

    Shared AR UPM 3.8.0

--- Runtime/Metadata.cs
+++ Runtime/Metadata.cs
@@ -4,6 +4,6 @@ namespace Niantic.Lightship.SharedAR.Settings
 {
     public static class Metadata
     {
-        public const string SharedArVersion = "3.7.0-2408080827";
+        public const string SharedArVersion = "3.8.0-2409081646";
     }
 }
