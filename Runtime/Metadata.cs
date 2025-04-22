8fcd22bcb1a8d451f9921fdb35934ef04edc47c9
koichi Mori <kmori@nianticlabs.com>
2025-04-22 09:33:03 -0700

    ARDK Shared AR UPM 3.13.0

--- Runtime/Metadata.cs
+++ Runtime/Metadata.cs
@@ -1,9 +1,9 @@
-// Copyright 2022-2024 Niantic.
+// Copyright 2022-2025 Niantic.
 
 namespace Niantic.Lightship.SharedAR.Settings
 {
     public static class Metadata
     {
-        public const string SharedArVersion = "3.12.0-2502182052";
+        public const string SharedArVersion = "3.13.0-2504092018";
     }
 }
